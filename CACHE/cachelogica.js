b03980e268a2320caf25b6c09887f1217c3681de
Jesus Zuniga Mendez <jesuszunigame@gmail.com>
2023-06-28 13:54:03 -0600

    version con cahce funcional

--- CACHE/cachelogica.js
+++ CACHE/cachelogica.js
@@ -41,16 +41,12 @@ function simularCadaCache(verComportamiento){
         }
       }
     }
-    //se construyen los objetos cache segun la configuracion
-    var L1 = new Cache(configuracionL1,"L1");
-    var L2 = new Cache(configuracionL2,"L2");
-    var L3 = new Cache(configuracionL3,"L3");
-    var arrayCaches = [L1,L2,L3];
-    simularConfiguracion(verComportamiento,titulo,arrayCaches,cantidadConfiguraciones);
+    var arrayConfiguraciones = [configuracionL1,configuracionL2,configuracionL3];
+    simularConfiguracion(verComportamiento,titulo,cantidadConfiguraciones,arrayConfiguraciones);
   });
 }
 //esta funcion realiza las simulaciones para cada trace
-async function simularConfiguracion(verComportamiento,titulo,arrayCaches,cantidadConfiguraciones) {
+async function simularConfiguracion(verComportamiento,titulo,cantidadConfiguraciones,arrayConfiguraciones) {
   // Muestra la pantalla de carga
   //se hace un array con los tracer que se seleccionaron
   console.time('tiempo');
@@ -59,7 +55,7 @@ async function simularConfiguracion(verComportamiento,titulo,arrayCaches,cantida
   const arraydetraces = Array.from(checkboxes).map((checkbox) => checkbox.getAttribute("value"));
   const totalTraces = arraydetraces.length;
   console.log("Traces a simular para cada configuracion",totalTraces);
-  const promesas = arraydetraces.map((trace) => simularTrace(verComportamiento,arrayCaches,cantidadConfiguraciones,trace,totalTraces));
+  const promesas = arraydetraces.map((trace) => simularTrace(verComportamiento,cantidadConfiguraciones,trace,totalTraces,arrayConfiguraciones));
   const resultados = await Promise.all(promesas);
   //cierra la pantalla de carga
   document.getElementById('pantallaCarga').style.display = 'none';
@@ -67,8 +63,12 @@ async function simularConfiguracion(verComportamiento,titulo,arrayCaches,cantida
   console.timeEnd('tiempo');
   //aqui se hace hace el proceso de descarga de los resultados de todas las simulaciones      
   // Crear un enlace de descarga
+  var ArchivoDescargar = "Trace,Misses,MissRate,MissesLectura,MissRateLectura,MissesEscritura,MissRateEscritura\n";
+  for (let i = 0; i < resultados.length; i++){
+    ArchivoDescargar = ArchivoDescargar + resultados[i];
+  }
   var enlaceDescarga = document.createElement('a');
-  enlaceDescarga.href = URL.createObjectURL(new Blob([resultados], { type: 'text/plain' }));
+  enlaceDescarga.href = URL.createObjectURL(new Blob([ArchivoDescargar], { type: 'text/plain' }));
   enlaceDescarga.download = titulo + '.csv';
   //enlaceDescarga.download = archivoGz.slice(0, -3);
   // Simular el clic en el enlace de descarga
@@ -79,7 +79,7 @@ async function simularConfiguracion(verComportamiento,titulo,arrayCaches,cantida
 }
 
 //esta funcion descomprime cada trace y lo simula
-function simularTrace(verComportamiento,arrayCaches,cantidadConfiguraciones,trace,totalTraces) {
+function simularTrace(verComportamiento,cantidadConfiguraciones,trace,totalTraces,arrayConfiguraciones) {
   // Obtener referencia a la barra de carga
   const barraProgreso = document.getElementById('barraRelleno');
   // Mostrar la barra de carg
@@ -100,7 +100,7 @@ function simularTrace(verComportamiento,arrayCaches,cantidadConfiguraciones,trac
           //simulacion de cada configuracion de cache almacenada en tabla
           // Utilizar pako para descomprimir el contenido del archivo .gz 
           var descomprimido = pako.inflate(contenidoGz, { to: 'string' });
-          const resultado = obtenerResultado(verComportamiento,arrayCaches,trace,descomprimido);
+          const resultado = obtenerResultado(verComportamiento,trace,descomprimido,arrayConfiguraciones);
           resolve(resultado);
           //console.log(trace);
           promesasCompletadas++;
@@ -122,16 +122,20 @@ function simularTrace(verComportamiento,arrayCaches,cantidadConfiguraciones,trac
   });
 }
 
-function obtenerResultado(verComportamiento,arrayCaches,trace,descomprimido) {
-  console.log("Resultados: ",trace, "# ", (promesasCompletadas+1));
+function obtenerResultado(verComportamiento,trace,descomprimido,arrayConfiguraciones) {
+  console.log("%c Resultados: ", 'color: red;' ,trace, "# ", (promesasCompletadas+1));
   //variable que alcacenara todos los resultados
   var resultado = "";
   //se recorre el archivo por lineas y se combierte a binario
   var nombreTrace = trace.slice(0, -12);
   var porLineas = descomprimido.split("\n");
-  var L1 = arrayCaches[0];
+  /*var L1 = arrayCaches[0];
   var L2 = arrayCaches[1];
-  var L3 = arrayCaches[2];
+  var L3 = arrayCaches[2];*/
+  //console.log("se hacen los cache");
+  let L1 = new Cache(arrayConfiguraciones[0],"L1");
+  let L2 = new Cache(arrayConfiguraciones[1],"L2");
+  let L3 = new Cache(arrayConfiguraciones[2],"L3");
   // Recorrer cada línea del array
   //se recorre el archivo linea por linea
   var contadorLineas = 0;
@@ -155,10 +159,16 @@ function obtenerResultado(verComportamiento,arrayCaches,trace,descomprimido) {
       console.log("%cERROR: No existe el primer nivel de cache simulacion invalida", 'color: red;');
     }
   });
-  resultado = L1.totalaccesos + ",";
-  resultado = resultado + ((L1.totalmisses * 100) / L1.totalaccesos);
-  L1.limpiarCache();
-  return nombreTrace + "," + resultado + '\n';
+  resultado = resultado + nombreTrace + ",";
+  resultado = resultado + L1.totalmisses + ",";
+  resultado = resultado + ((L1.totalmisses * 100) / L1.totalaccesos) + ",";
+  resultado = resultado + L1.totalmisseslectura + ",";
+  resultado = resultado + ((L1.totalmisseslectura * 100) / L1.totallecturas) + ",";
+  resultado = resultado + L1.totalmissesescritura + ",";
+  resultado = resultado + ((L1.totalmissesescritura * 100) / L1.totalescrituras) + "\n";  
+  //console.log(L1.matriz);
+  //L1.limpiarCache();
+  return resultado;
 }
 
 
@@ -206,38 +216,39 @@ class Cache {
       }while(seguir);
       //se caclula el tamanio del tag
       this.tamniotag = this.tamaniodirecciones - this.tamanioIndex - this.tamaniobyteOffset;
-      console.log("tag",this.tamniotag);
-      console.log("tamanioIndex",this.tamanioIndex);
-      console.log("tamaniobyteOffset",this.tamaniobyteOffset);
+      //console.log("tag",this.tamniotag);
+      //console.log("tamanioIndex",this.tamanioIndex);
+      //console.log("tamaniobyteOffset",this.tamaniobyteOffset);
       //se hace la tabla que representará al cache
       this.filas = Math.pow(2, this.tamanioIndex); // Número de filas
       //this.filas = 3;
       // Llenar la matriz con strings
-      this.espacios=["V","tag","data","LRU"];
+      this.espacios=["Idefecto","tagdefecto","datadefecto",1];
       this.columnas = 1 + (this.espacios.length*parseInt(this.asociatividad)); // Número de columnas, se almacena 
       // Crear una matriz vacía
       this.matriz = [];
       for (let i = 0; i < this.filas; i++) { //filas
         this.matriz[i] = [];
         var way=0;
+        var contadorReemplazo = 1;
         for (let j = 0; j < this.columnas; j++) { //columnas
           if (j == 0){
             //var indexGuardar = this.completarbinario(i.toString(2),this.tamanioIndex);
             var indexGuardar = i.toString(2).padStart(this.tamanioIndex, '0');
             this.matriz[i][j] = indexGuardar; //se almacena el index
           }else{
-            this.matriz[i][j] = this.espacios[way];
             if (way == (this.espacios.length - 1)){
+              this.matriz[i][j] = contadorReemplazo;
               way = 0;
+              contadorReemplazo++;
             }else{
+              this.matriz[i][j] = this.espacios[way];
               way++;
             }
           }          
         }
       }
-
-      this.matrizCopia = this.matriz;
-      //console.log(this.matriz);
+      //console.log("matriz en cero ",this.matriz[0].toString());
       //se inicializan las variables de resultados
       //totales
       this.totalaccesos = 0;
@@ -257,13 +268,11 @@ class Cache {
       this.existe = false;
     }
   }
-
   valido() {
       return this.existe;
   }
   
   acceder(tipo,binario) {
-    this.totalaccesos++;
     var Esmiss = true;
     //variables para encontrar el inico y final
     var inicio = 1;
@@ -274,41 +283,207 @@ class Cache {
     inicio = final + 1;
     final = inicio + this.tamanioIndex - 1;
     var index = binario.substring((inicio-1), final);
-    //se saca el inde;
+    //se saca el byteoffset;
     inicio = final + 1;
     final = inicio + this.tamaniobyteOffset - 1;
     var byteOffse = binario.substring((inicio-1), final);
     //se combierte el index a entero para poder ubicar la posicion de la tabla
     var indexEntero = parseInt(index, 2);
+    //se hace una comprobacion de el index versus el de la tabla
     if (this.matriz[indexEntero][0] == index){
+      //se rqgistra el acceso que se realizaraá
+      this.totalaccesos++;
+      var imprimir = false;
+      var inferiormuestreoHits = 995000;
+      var inicioInstruccionesImprimir = 50000;
+      var finInstruccionesImprimir = 50005;
+      if ((this.totalaccesos >= inicioInstruccionesImprimir) && (this.totalaccesos <= finInstruccionesImprimir)){
+        imprimir = true;
+        console.log('%c acceso ','color: blue', this.totalaccesos.toString());
+      }else{
+        imprimir = false;
+      }      
+      if (imprimir){
+        console.log("matriz sin tratar ");
+        console.log(this.matriz[indexEntero].toString());
+        console.log("datos de acceso");
+        console.log("tipo",tipo.toString());
+        console.log("binario",binario.toString());
+        console.log("index",index.toString());
+        console.log("tag",tag.toString());
+        console.log("byteofsst",byteOffse.toString());
+      }
+      if ((tipo == "w") || (tipo == "W")){
+        this.totalescrituras++;
+        if (imprimir){
+          console.log("escritura suma");
+          console.log(this.totalescrituras.toString());
+        }
+      }else if ((tipo == "r") || (tipo == "R")){   
+        this.totallecturas++;
+        if (imprimir){
+          console.log("lecturas suma");
+          console.log(this.totallecturas.toString());
+        }        
+      }else{
+        console.log("Este es el else de la suma de lecturas y escrituras no deberia de salir")        
+      }
+      //esta variable permite controlar el retiro de datoss de cada way
       var columnaRevisar = 1;
-      for (let c = 1; c <= this.asociatividad; c++) { //ways, recorre cada uno de los tres datos por ways  
-        var validAlmacenado = this.matriz[indexEntero][columnaRevisar];
-        var tagAlmacenado = this.matriz[indexEntero][columnaRevisar+1];
-        var dataAlmacenado = this.matriz[indexEntero][columnaRevisar+2];
-        var LRUAlmacenado = this.matriz[indexEntero][columnaRevisar+3];
-        columnaRevisar = columnaRevisar + 4;  
-        if ((tipo == "w") || (tipo == "W")){
-          if (tagAlmacenado == tag){
-          }else{
-            this.totalmisses ++;
-          }
-        }else if ((tipo == "r") || (tipo == "R")){         
-          if (tagAlmacenado == tag){
-          }else{
-            this.totalmisses ++;
-          }
-        }
+      //se asume que este acceso va a ser un  miss
+      var miss = true;
+      //esta variable guarda los reemplazoa almacenados
+      var validAlmacenado ="";
+      var tagAlmacenado ="";
+      var dataAlmacenado ="";
+      var reemplazoAlmacenado =0;
+      var columnaDondeEstaReemplazo =0; 
+      //se recorren todos los ways para determinar si es un hit o un miss
+      for (let c = 1; c <= this.asociatividad; c++) {
+        //se retiran los datos de cada way
+        validAlmacenado = this.matriz[indexEntero][columnaRevisar];
+        tagAlmacenado = this.matriz[indexEntero][columnaRevisar+1];
+        dataAlmacenado = this.matriz[indexEntero][columnaRevisar+2];
+        reemplazoAlmacenado = this.matriz[indexEntero][columnaRevisar+3];
+        columnaDondeEstaReemplazo = columnaRevisar+3;
+        columnaRevisar = columnaRevisar + 4;
         /*if (this.totalaccesos == 1000){
-            console.log("validAlmacenado",validAlmacenado);
-            console.log("tagAlmacenado",tagAlmacenado);
-            console.log("dataAlmacenado",dataAlmacenado);
-            console.log("LRUAlmacenado",LRUAlmacenado);
-          }*/       
+          console.log("validAlmacenado",validAlmacenado);
+          console.log("tagAlmacenado",tagAlmacenado);
+          console.log("dataAlmacenado",dataAlmacenado);
+          console.log("LRUAlmacenado",ReemplazoAlmacenado);
+        }*/
+        if (tagAlmacenado == tag){
+          if (this.totalaccesos> inferiormuestreoHits){
+            imprimir = true;
+            console.log('%c acceso ','color: yellow', this.totalaccesos.toString());
+          }
+          if (imprimir){
+            console.log("fue hit");
+            console.log(tagAlmacenado.toString());
+            console.log(tag.toString());
+            console.log(tipo);
+            console.log("actualizamos reemplazo");
+            console.log(this.matriz[indexEntero].toString());
+          } 
+          //si esta, el dato en algun way comprobando el tag quiere decir que no es un miss
+          miss = false;
+          //se registra en cual tag se encontro ya que si se encontro es el 
+          //way que se utilizo mas recientemente
+          //se actualiza el indice de reempazo
+          this.actualizarReemplazo(indexEntero,reemplazoAlmacenado, columnaDondeEstaReemplazo);
+          //como se encontro se detiene el ciclo
+          if (imprimir){
+            console.log("reemplazo actualizado");
+            console.log(this.matriz[indexEntero].toString());
+          } 
+          break;
         }
-      }    
+      }
+      //se verifica el estado de la variable miss, si es un miss se comprueba que tipo de acceso era
+      //y se suma el contador correspondiente 
+      if (miss == true){
+        if (this.totalaccesos> inferiormuestreoHits){
+          imprimir = true;
+          console.log('%c acceso ','color: green', this.totalaccesos.toString());
+        }
+        if ((tipo == "w") || (tipo == "W")){
+          //si es un miss y era escritura se suman los contadores
+          this.totalmisses++;
+          this.totalmissesescritura++; 
+          if (imprimir){
+            console.log("fue mis");
+            console.log(tagAlmacenado.toString());
+            console.log(tag.toString());
+            console.log(tipo);
+            console.log("escribimos");
+            console.log(this.matriz[indexEntero].toString());
+          }  
+          //como es un miss hay que escribir en cache
+          this.escribir(indexEntero,"V",tag,binario);
+          if (imprimir){
+            console.log("escrito");
+            console.log(this.matriz[indexEntero].toString());
+          }  
+        }else if ((tipo == "r") || (tipo == "R")){   
+          //si es un miss y era lectura se suman los contadores
+          this.totalmisses++;
+          this.totalmisseslectura++; 
+          if (imprimir){
+            console.log("fue mis con ");
+            console.log(tagAlmacenado);
+            console.log(tag.toString());
+            console.log(tipo);
+            console.log("leemos no hacemos nada");
+            console.log(this.matriz[indexEntero].toString());
+          }  
+          this.escribir(indexEntero,"V",tag,binario);
+          if (imprimir){
+            console.log("no se hizo nada");
+            console.log(this.matriz[indexEntero].toString());
+          } 
+        }else{
+          console.log("Este es el else de las lecturas y escrituras no deberia de salir")        
+        }
+      }
+    }else{
+      console.log("Este es el if del index no deberia de salir");
+    }              
     return Esmiss;
   }
+
+  //funcion que escribe en el cache
+  escribir(indexEntero,valid,tag,binario) {
+    if (this.politicaReemplazo == "LRU"){
+      var columnaRevisar = 4;
+      var valorMinimo = 1;
+      var reemplazoAlmacenado =0;
+      for (let c = 1; c <= this.asociatividad; c++) {
+        if (this.matriz[indexEntero][columnaRevisar] == valorMinimo){
+          this.matriz[indexEntero][columnaRevisar-3] = valid;
+          this.matriz[indexEntero][columnaRevisar-2] = tag;
+          this.matriz[indexEntero][columnaRevisar-1] = binario;
+          this.actualizarReemplazo(indexEntero,valorMinimo, columnaRevisar);      
+          break;
+        }
+        columnaRevisar = columnaRevisar + 4;
+      }
+    }else if (this.politicaReemplazo == "Aleatorio"){
+      const min = 1;
+      const max = this.asociatividad;
+      const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+      var columnainicio = ((randomNum -1)*4)+1;
+      this.matriz[indexEntero][columnainicio] = valid;
+      this.matriz[indexEntero][columnaRevisar+1] = tag;
+      this.matriz[indexEntero][columnaRevisar+2] = binario;
+    }else{
+      console.log("else del actualizarreemplazo no deberia de salir");
+    }
+  }
+  //funcion que actualiza cual es el way mas recientemente utilizado
+  actualizarReemplazo(indexEntero,reemplazoAlmacenado, columnaDondeEstaReemplazo){
+    //si el reemplazo es LRU coloca en la casilla del index,way el valor que indica mas reciente  
+    if (this.politicaReemplazo == "LRU"){
+      var valorMaximo = +this.asociatividad;
+      var valorMinimo = 1;
+      var columnaRevisar = 4;
+      for (let c = 1; c <= this.asociatividad; c++) {
+        if (this.matriz[indexEntero][columnaRevisar]>reemplazoAlmacenado){
+          this.matriz[indexEntero][columnaRevisar] = this.matriz[indexEntero][columnaRevisar] - 1;
+          if ((this.matriz[indexEntero][columnaRevisar]) < valorMinimo){
+            console.log("el errorsote");
+          }
+        }
+        columnaRevisar = columnaRevisar + 4;
+      }
+      this.matriz[indexEntero][columnaDondeEstaReemplazo] = valorMaximo;
+    }else if (this.politicaReemplazo == "Aleatorio"){
+      //codigo de aleatoio
+    }else{
+      console.log("else del actualizarreemplazo no deberia de salir");
+    }
+  }
+
 
   limpiarCache(){
     //se inicializan las variables de resultados
@@ -325,26 +500,22 @@ class Cache {
     this.totalmissesescritura = 0;
     this.totalmissrateescritura = 0;        
     //se limpia la tabla
-    this.matriz = this.matrizCopia;
-    // Llenar la matriz con strings
-    /*for (let i = 0; i < this.filas; i++) { //filas
-      this.matriz[i] = [];
+    for (let i = 0; i < this.filas; i++) { //filas
       var way=0;
       for (let j = 0; j < this.columnas; j++) { //columnas
         if (j == 0){
-          this.matriz[i][j] = i.toString(2); //se almacena el index
+          var indexGuardar = i.toString(2).padStart(this.tamanioIndex, '0');
+          this.matriz[i][j] = indexGuardar; //se almacena el index
         }else{
           this.matriz[i][j] = this.espacios[way];
-          if (way == 2){
+          if (way == (this.espacios.length - 1)){
             way = 0;
           }else{
             way++;
           }
         }          
       }
-    }*/
-    //console.log("se vacia el cache");
-    //console.log(this.matriz);
+    }
   }
 
 
